2715ecbb150df23634567c3c2e3a1e6c10b43294
Jakub Jarosiński <dzejkopixxx@gmail.com>
2022-03-18 20:42:40 +0100

    odejmowanie zaimplementowane

--- test.py
+++ test.py
@@ -5,7 +5,14 @@ def dodaj(a,b):
 	wynik = float(a) + float(b)
 	return wynik
 
+def odejmi(a,b):
+	wynik = float(a) - float(b)
+	return wynik
+
+
 pierwsza = input()
 druga = input()
 
 print(dodaj(pierwsza,druga))
+print(odejmi(pierwsza,druga))
+
